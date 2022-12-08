82a4523cfcf63858a074ffcac7094adb2d76fd40
Emanuel <Emanuel@Emanuels-MacBook-Pro.local>
2022-12-08 23:49:03 +0200

    l110

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+// Lesson 110
+
+const game = {
+  team1: "Bayern MUnich",
+  team2: "Borrussia Dortmund",
+  playesrs: [
+    [
+      "Neur",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnabry",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnabry", "Lewandowski", "Hummels"],
+  date: "No 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
